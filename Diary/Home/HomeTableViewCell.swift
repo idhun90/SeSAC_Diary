727cede1eed5900299ab6d156c67303589651381
idhun90 <idhun90@naver.com>
2022-08-26 17:49:25 +0900

    Cell 파일에서 데이터 관리 위해 loadImageFromDocument(fileName:) 메소드 추가

--- Diary/Home/HomeTableViewCell.swift
+++ Diary/Home/HomeTableViewCell.swift
@@ -57,6 +57,19 @@ class HomeTableViewCell: BaseTableViewCell {
         titleLabel.text = data.title
         dateLabel.text = data.date.formatted()
         contentLabel.text = data.content
+        homeImageView.image = loadImageFromDocument(fileName: "\(data.objectId).jpg")
+        
+    }
+    
+    func loadImageFromDocument(fileName: String) -> UIImage? {
+        guard let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { return nil } // 세부 경로 접근
+        let fileURL = documentDirectory.appendingPathComponent(fileName) // 이미지를 저장할 위치
+        
+        if FileManager.default.fileExists(atPath: fileURL.path) { // 해당 경로에 실제 이미지 존재 유무 파악
+            return UIImage(contentsOfFile: fileURL.path)
+        } else {
+            return UIImage(systemName: "star.fill")
+        }
     }
     
     override func configure() {
